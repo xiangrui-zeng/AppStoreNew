77b12f3376f57e47bea83e8edcaf2aa57209c456
ZengXR <xiangrui.zeng@gmail.com>
2013-12-24 09:45:56 +0800

    index

--- routes/index.js
+++ routes/index.js
@@ -148,6 +148,7 @@ exports.guiding = function (app) {
   // 获取图片
   app.get('/picture/:id', function (req, res) {
     file.getImage(req, res);
+  });
 
   app.get('/file/download.json', function (req, res) {
     download.create(req,res,function(){
