98b13944078c8a390183b4fb7c3b69c680d806d7
yangxiaomu <ytxiaomu@gmail.com>
2013-12-31 17:15:30 +0800

    app数据字段设计完成

--- modules/mod_app.js
+++ modules/mod_app.js
@@ -4,51 +4,43 @@ var mongo       = smart.util.mongoose
 
 var App = new schema({
 
-    name: {type: String}  //名称
-    , description: {type: String}           //详细介绍
-    , memo: {type: String}                  //简介
-    , release_note: {type: String}           //
-    , appType: {type: String}               //应用设备的类型：iphone、ipad、winphone
-    , permission: {
-        admin: [String]                     //管理者权限
-        , edit: [String]                      //编集权限
-        , view: [String]                      //阅览权限
-        , download: [String]                  //下载权限
-    }, icon: {                               //图标
-        big: {type: String}, small: {type: String}
-    }, screenshot: [String]                  //截图（最多5张）
-    , video: {type: String}                  //视频
-    , category: [String]                    //分类
-    , version: {type: String}               //版本
-    , downloadId: {type: String}            //下载Id
-    , plistDownloadId : {type:String}       // plist  下载id
-    , pptfile: {type: String}            //pptfile 演示文件
-    , open_date: {type: Date}             //公开日期
-    , expire_date: {type: Date}           //过期
-    , create_date: {type: Date}           //创建日期
-    , create_user: {type: String}           //创建者
-    , update_date: {type: Date}           //更新日期
-    , update_user: {type: String}           //更新者
-    , require: {
-        os: {type: String}                  //系统要求
-        , device: {type: String}              //设备要求
-        , server: {type: String}              //服务器要求
-    }, size: {type: Number}                      //大小
-    , status: {type: Number}                 // 状态：-1、无效app 0、未公开 1、社内公开 2、社外限定公开 3、社外任意公开
-    , editing : {type:Number}               //编辑中   0 、表示编辑中     1、表示编辑完成
-    , rank: {type: Number}
-    , support :{type:String}
-    , notice : {type:String}
-    , editstep :{type:Number}           //编辑进行的状态
-    , bundle_identifier :{type:String}
-    , bundle_version    :{type:String}
-    , kind              :{type:String}
-    , title             :{type:String}
-    , admin_list : {type: schema.Types.Mixed}          //返回前台的 管理员信息
-    , edit_list :    {type: schema.Types.Mixed}        //返回前台的 编辑者信息
-    , view_list :  {type: schema.Types.Mixed}          //返回前台的 浏览者信息
-    , download_list :  {type: schema.Types.Mixed}      //返回前台的 下载者信息
-    , downloadCount :{type: Number}
+      name          : {type: String, description:"名称"}
+    , description   : {type: String, description:"详细信息"}
+    , release_note  : {type: String, description:"更新信息"}
+    , appType       : {type: String, description:"设备的类型：OS、android、PC "}
+    , permission    : {
+          admin      : [String]
+        , edit       : [String]
+        , view       : [String]
+        , download   : [String]
+        , description:"权限??"}
+    , icon          : {
+        big          : {type: String}
+      , small        : {type: String}, description:"大小图标"}
+    , screenshot      : [String]
+    , category        : [String]
+    , version         : {type: String, description:"版本"}
+    , downloadId      : {type: String, description:"下载id"}
+    , plistDownloadId : {type:String, description:"pList"}
+    , open_date       : {type: Date, description:"公开日期"}
+    , expire_date     : {type: Date, description:"过期时间"}
+    , create_date     : {type: Date, description:"创建时间"}
+    , create_user     : {type: String, description:"创建者"}
+    , update_date     : {type: Date, description:"更新日期"}
+    , update_user     : {type: String, description:"更新者"}
+    , copyright       : {type: String, description:"版本"}
+    , require         : {
+        os: {type: String}
+      , device: {type: String}
+      , description:"系统 ios7.0 、设备要求iphone 5S"}
+    , size            : {type: Number, description:"大小size"}
+    , status          : {type: Number, description:"状态： 0、未申请 1、待审核 2、公开中 3、审核未通过 4、无效 ",default:0}
+    , rank            : {type: Number, description:"评分分数",default: 0}
+    , rankcount       : {type: Number, description:"评分次数",default: 0}
+    , bundle_identifier :{type:String, description:"plist  标识"}
+    , bundle_version  :{type:String, description:"plist"}
+    , kind            :{type:String, description:"种类"}
+    , downloadCount   :{type: Number, description:"下载数量"}
 });
 
 function model() {
