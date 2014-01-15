8caf2c05a7af872d8e8333251f3b92c7dda99a7a
DACchenda <136085828@qq.com>
2014-01-15 21:23:58 +0800

    添加apk下载链接和exe下载连接，修改plist链接的名称，使名称更具有实际意义

--- routes/api_download.js
+++ routes/api_download.js
@@ -4,11 +4,23 @@ var download = require("../apis/download");
 
 exports.guiding = function(app){
 
+  //获取plist
   app.get("/download/:app_id/:user_id/app.plist", function(req, res) {
     download.getPlist(req, res);
   });
 
-  app.get("/download/:app_id/:user_id/app.ipa", function(req, res) {
+  //获取ipa文件
+  app.get("/download/:app_id/:user_id/IosApp.ipa", function(req, res) {
     download.getIpaFile(req, res);
   });
+
+  //获取apk文件
+	app.get("/download/:app_id/:user_id/AndriodApp.apk", function(req, res) {
+		download.getIpaFile(req, res);
+	});
+
+	//获取exe文件
+	app.get("/download/:app_id/:user_id/PCWebApp.exe", function(req, res) {
+		download.getIpaFile(req, res);
+	});
 };
